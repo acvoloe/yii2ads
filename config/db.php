e470240eea1c7d57cd626d7db2fcfc0dabcc006f
Roman Varda <acvoloe@gmail.com>
2017-02-02 14:05:50 +0700

    UPDATE .gitignore config database

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2ads',
-    'username' => 'yii2ads',
-    'password' => '7777777',
-    'charset' => 'utf8',
-];
